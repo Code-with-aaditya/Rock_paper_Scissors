6d271c7651f7dec2f9e11f23dd5a05b1e26cd8ef
Aditya Kumar <aditya147012@gmail.com>
2023-08-28 22:32:11 +0530

    This is my main java file

--- game.java
+++ game.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class game {
     public static void main(String[] args) {
+        System.out.println("Rock, Paper, Scissors");
+        
         Scanner sc = new Scanner(System.in);
         Random random = new Random(); // Generate random number -> upto 2
         int computerInput = random.nextInt(3);
